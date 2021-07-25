179b19a91ce15b6498eaccfbaf9a510a6b4c411a
pradiph31 <pradip.hamal31@gmail.com>
2021-07-25 15:36:14 -0700

    Created SeedData

---
+++ FirstRazor/Models/SeedData.cs
@@ -0,0 +1,21 @@
+﻿using FirstRazor.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstRazor.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new FirstRazorContext(
+                serviceProvider.GetRequiredService<DbContextOptions<FirstRazorContext>>()))
+            {
+            }
+        }
+    }
+}

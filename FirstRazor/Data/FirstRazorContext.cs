dd33bd6abe27f23bffc1b5fe1d9d4ca5ace615b7
pradiph31 <pradip.hamal31@gmail.com>
2021-07-25 11:38:26 -0700

    Scaffold Razor Pages with EF Core

---
+++ FirstRazor/Data/FirstRazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstRazor.Models;
+
+namespace FirstRazor.Data
+{
+    public class FirstRazorContext : DbContext
+    {
+        public FirstRazorContext (DbContextOptions<FirstRazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstRazor.Models.Movie> Movie { get; set; }
+    }
+}

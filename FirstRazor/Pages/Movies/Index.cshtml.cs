b9a9bcfbaf059d3e05263ae0ddc10de793a83be6
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 14:20:49 -0700

    Query to get all genres and put them in a selectlist

--- FirstRazor/Pages/Movies/Index.cshtml.cs
+++ FirstRazor/Pages/Movies/Index.cshtml.cs
@@ -29,12 +29,23 @@ namespace FirstRazor.Pages.Movies
 
         public async Task OnGetAsync()
         {
+            IQueryable<String> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
         }
     }

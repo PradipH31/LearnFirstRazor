16cfd6c387ea68ba49cc434c01deb5f065214f04
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 14:51:36 -0700

    Added Migrations for validation

--- FirstRazor/Migrations/20210726215109_New_DataAnnotations.Designer.cs
+++ FirstRazor/Migrations/20210726215109_New_DataAnnotations.Designer.cs
@@ -4,14 +4,16 @@ using FirstRazor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FirstRazor.Migrations
 {
     [DbContext(typeof(FirstRazorContext))]
-    partial class FirstRazorContextModelSnapshot : ModelSnapshot
+    [Migration("20210726215109_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
